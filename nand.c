1e237c7ebe3df782f01365e0d9e1820ec3a6c85d
Sean Cross <xobs@xoblo.gs>
2012-12-01 13:50:58 -0300

    nand: Remove dependency on /dev/kmem
    For prototyping we read directly from GPIO registers.  This is no
    longer required, so use the standard Linux gpiolib register acccess
    mechanisms instead.

--- nand.c
+++ nand.c
@@ -10,12 +10,6 @@
 #include "sd.h"
 #include "gpio.h"
 
-static int fd = 0;
-static volatile int   *mem_32 = 0;
-static volatile short *mem_16 = 0;
-static volatile char  *mem_8  = 0;
-static int *prev_mem_range = 0;
-
 static int data_pins[] = {
 	45, // CAM_D[0]
 	44, // CAM_D[1]
@@ -44,101 +38,21 @@ static int bank_select_pins[] = {
 #define GPIO_PATH "/sys/class/gpio"
 #define GET_NEW_SAMPLE_PIN 54
 
-static int read_kernel_memory(long offset, int virtualized, int size) {
-    int result;
-
-    int *mem_range = (int *)(offset & ~0xFFFF);
-    if( mem_range != prev_mem_range ) {
-        prev_mem_range = mem_range;
-
-        if(mem_32)
-            munmap((void *)mem_32, 0xFFFF);
-        if(fd)
-            close(fd);
-
-        if(virtualized) {
-            fd = open("/dev/kmem", O_RDWR);
-            if( fd < 0 ) {
-                perror("Unable to open /dev/kmem");
-                fd = 0;
-                return -1;
-            }
-        }
-        else {
-            fd = open("/dev/mem", O_RDWR);
-            if( fd < 0 ) {
-                perror("Unable to open /dev/mem");
-                fd = 0;
-                return -1;
-            }
-        }
-
-        mem_32 = mmap(0, 0xffff, PROT_READ | PROT_WRITE, MAP_SHARED, fd, offset&~0xFFFF);
-        if( -1 == (long)mem_32 ) {
-            perror("Unable to mmap file");
-
-            if( -1 == close(fd) )
-                perror("Also couldn't close file");
-
-            fd=0;
-            return -1;
-        }
-        mem_16 = (short *)mem_32;
-        mem_8  = (char  *)mem_32;
-    }
-
-    int scaled_offset = (offset-(offset&~0xFFFF));
-    if(size==1)
-        result = mem_8[scaled_offset/sizeof(char)];
-    else if(size==2)
-        result = mem_16[scaled_offset/sizeof(short)];
-    else
-        result = mem_32[scaled_offset/sizeof(long)];
-
-    return result;
-}
-
-static int write_kernel_memory(long offset, long value, int virtualized, int size) {
-    int old_value = read_kernel_memory(offset, virtualized, size);
-    int scaled_offset = (offset-(offset&~0xFFFF));
-    if(size==1)
-        mem_8[scaled_offset/sizeof(char)]   = value;
-    else if(size==2)
-        mem_16[scaled_offset/sizeof(short)] = value;
-    else
-        mem_32[scaled_offset/sizeof(long)]  = value;
-    return old_value;
-}
-
-static int get_gpio(int gpio) {
-	long gpio_offset;
-	if (gpio < 32)
-		gpio_offset = 0xd4019000;
-	else if (gpio < 64)
-		gpio_offset = 0xd4019004;
-	else if (gpio < 96)
-		gpio_offset = 0xd4019008;
-	else
-		gpio_offset = 0xd4019100;
-	return !!(read_kernel_memory(gpio_offset, 0, 4) & (1<<(gpio & 0x1f)));
-}
 
 int nand_get_new_sample(struct sd *st, uint8_t bytes[8]) {
 	uint8_t data[16];
-	long set_register = 0xd401901c;
-	long clr_register = 0xd4019028;
 	int tries;
 	int bank;
 	int i;
 	int ret;
 	usleep(2);
-	write_kernel_memory(clr_register, 1<<(GET_NEW_SAMPLE_PIN&0x1f), 0, 4);
+	gpio_set_value(GET_NEW_SAMPLE_PIN, 0);
 	usleep(2);
-	write_kernel_memory(set_register, 1<<(GET_NEW_SAMPLE_PIN&0x1f), 0, 4);
+	gpio_set_value(GET_NEW_SAMPLE_PIN, 1);
 
 	ret = -1;
 	for (tries=0; tries<2; tries++) {
-		if (get_gpio(60)) {
+		if (gpio_get_value(60)) {
 			DBG("Gpio went high after %d tries", tries);
 			ret = 0;
 			break;
@@ -152,7 +66,7 @@ int nand_get_new_sample(struct sd *st, uint8_t bytes[8]) {
 		gpio_set_value(bank_select_pins[1], !!(bank&2));
 		usleep(2);
 		for (i=0; i<sizeof(data_pins)/sizeof(*data_pins); i++)
-			data[i] = get_gpio(data_pins[i]);
+			data[i] = gpio_get_value(data_pins[i]);
 		bytes[bank*2+0] = (data[0]<<0)
 				| (data[1]<<1)
 				| (data[2]<<2)
@@ -175,13 +89,15 @@ int nand_get_new_sample(struct sd *st, uint8_t bytes[8]) {
 }
 
 int nand_data_avail(struct sd *st) {
+/*
 	struct pollfd fdset[1];
 	fdset[0].fd = st->nand_fd;
 	fdset[0].events = POLLPRI;
-//	if (-1 == poll(fdset, 1, 3*1000)) {
-//		perror("Error polling");
-//	}
-	return get_gpio(DATA_READY_PIN);
+	if (-1 == poll(fdset, 1, 3*1000)) {
+		perror("Error polling");
+	}
+*/
+	return gpio_get_value(DATA_READY_PIN);
 }
 
 
@@ -189,6 +105,7 @@ int nand_init(struct sd *sd) {
 	int i;
 	char str[256];
 
+	/* Grab the "data ready pin", and open it so we can poll() */
 	gpio_export(DATA_READY_PIN);
 	gpio_set_direction(DATA_READY_PIN, GPIO_IN);
 	snprintf(str, sizeof(str)-1, "%s/gpio%d/value", GPIO_PATH, DATA_READY_PIN);
