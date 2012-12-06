540c69a51a02ae8fb5d92f941465e86256e14f55
Sean Cross <xobs@xoblo.gs>
2012-12-06 11:45:53 -0300

    packet: Add packet size to header
    We now add two bytes of packet size to the header of all commands.  This
    brings the base header size up to 11 bytes.  This change allows us to
    serialize the data stream to disk, and to skip over unknown packets
    that get added at a future time.

--- packet.c
+++ packet.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include "sd.h"
 
+
 enum FPGAFrequency {
 	FPGA_FREQUENCY = 125000000,
 };
@@ -25,18 +26,25 @@ enum PacketType {
 /* Generic packet header
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  1   | Packet type (as defined in WPacketType)
+ *     0   |  1   | Packet type (as defined in PacketType)
  *     1   |  4   | Seconds since reset
  *     5   |  4   | Nanoseconds since reset
+ *     9   |  2   | Header size
  */
-static int pkt_set_header(struct sd *sd, char *pkt, int type) {
-	long sec, nsec;
+#define PKT_HEADER_SIZE (1+4+4+2)
+
+static int pkt_set_header(struct sd *sd, char *pkt, int type, int size) {
+	long real_sec, real_nsec;
+	uint32_t sec, nsec;
+	uint16_t sz;
 	pkt[0] = type;
-	sd_get_elapsed(sd, &sec, &nsec);
-	sec = htonl(sec);
-	nsec = htonl(nsec);
+	sd_get_elapsed(sd, &real_sec, &real_nsec);
+	sec = htonl(real_sec);
+	nsec = htonl(real_nsec);
+	sz = htons(size);
 	memcpy(pkt+1, &sec, sizeof(sec));
 	memcpy(pkt+5, &nsec, sizeof(nsec));
+	memcpy(pkt+9, &sz, sizeof(sz));
 	return 0;
 }
 
@@ -48,12 +56,13 @@ static int pkt_set_header(struct sd *sd, char *pkt, int type) {
  *     1   |  4   | Seconds since reset
  *     5   |  4   | Nanoseconds since reset
  */
-static int pkt_set_header_fpga(struct sd *sd, char *pkt, uint32_t fpga_counter, int type) {
+static int pkt_set_header_fpga(struct sd *sd, char *pkt, uint32_t fpga_counter, int type, int size) {
 	uint32_t ticks = fpga_ticks(sd);
 	long long total_ticks = ticks*0x100000000LL + fpga_counter;
 	long long total_secs;
 	long long nsec_ticks;
 	uint32_t sec, nsec;
+	uint16_t sz;
 
 	total_secs = total_ticks / FPGA_FREQUENCY;
 	nsec_ticks = total_ticks - (total_secs * FPGA_FREQUENCY);
@@ -64,8 +73,10 @@ static int pkt_set_header_fpga(struct sd *sd, char *pkt, uint32_t fpga_counter, 
 	pkt[0] = type;
 	sec = htonl(sec);
 	nsec = htonl(nsec);
+	sz = htons(size);
 	memcpy(pkt+1, &sec, sizeof(sec));
 	memcpy(pkt+5, &nsec, sizeof(nsec));
+	memcpy(pkt+9, &sz, sizeof(sz));
 	return 0;
 }
 
@@ -73,21 +84,21 @@ static int pkt_set_header_fpga(struct sd *sd, char *pkt, uint32_t fpga_counter, 
 /* PKT_ERROR
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  4   | Error code
- *    13   | 512  | Textual error message (NULL-padded)
+ *     0   | 11   | Header
+ *    11   |  4   | Error code
+ *    15   | 512  | Textual error message (NULL-padded)
  */
 int pkt_send_error(struct sd *sd, uint32_t code, char *msg) {
-	char pkt[9+4+512];
+	char pkt[PKT_HEADER_SIZE+4+512];
 	uint32_t real_code;
 
 	bzero(pkt, sizeof(pkt));
 
-	pkt_set_header(sd, pkt, PACKET_ERROR);
+	pkt_set_header(sd, pkt, PACKET_ERROR, sizeof(pkt));
 	real_code = htonl(code);
-	memcpy(pkt+9, &real_code, sizeof(real_code));
+	memcpy(pkt+PKT_HEADER_SIZE+0, &real_code, sizeof(real_code));
 
-	strncpy(pkt+13, msg, 512-1);
+	strncpy(pkt+PKT_HEADER_SIZE+4, msg, 512-1);
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -96,18 +107,18 @@ int pkt_send_error(struct sd *sd, uint32_t code, char *msg) {
  * PACKET_NAND_CYCLE format (FPGA):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  1   | Data/Command pins
- *    10   |  1   | Bits [0..4] are ALE, CLE, WE, RE, and CS (in order)
- *    11   |  2   | Bits [0..9] are the unknown pins
+ *     0   | 11   | Header
+ *    11   |  1   | Data/Command pins
+ *    12   |  1   | Bits [0..4] are ALE, CLE, WE, RE, and CS (in order)
+ *    13   |  2   | Bits [0..9] are the unknown pins
  */
 int pkt_send_nand_cycle(struct sd *sd, uint32_t fpga_counter, uint8_t data, uint8_t ctrl, uint8_t unk[2]) {
-	char pkt[9+1+1+2];
-	pkt_set_header_fpga(sd, pkt, fpga_counter, PACKET_NAND_CYCLE);
-	pkt[9] = data;
-	pkt[10] = ctrl;
-	pkt[11] = unk[0];
-	pkt[12] = unk[1];
+	char pkt[PKT_HEADER_SIZE+1+1+2];
+	pkt_set_header_fpga(sd, pkt, fpga_counter, PACKET_NAND_CYCLE, sizeof(pkt));
+	pkt[PKT_HEADER_SIZE+0] = data;
+	pkt[PKT_HEADER_SIZE+1] = ctrl;
+	pkt[PKT_HEADER_SIZE+2] = unk[0];
+	pkt[PKT_HEADER_SIZE+3] = unk[1];
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -116,13 +127,13 @@ int pkt_send_nand_cycle(struct sd *sd, uint32_t fpga_counter, uint8_t data, uint
  * PACKET_SD_DATA format (CPU):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   | 512  | One block of data from the card
+ *     0   | 11   | Header
+ *    11   | 512  | One block of data from the card
  */
 int pkt_send_sd_data(struct sd *sd, uint8_t *block) {
-	char pkt[9+512];
-	pkt_set_header(sd, pkt, PACKET_SD_DATA);
-	memcpy(pkt+9, block, 512);
+	char pkt[PKT_HEADER_SIZE+512];
+	pkt_set_header(sd, pkt, PACKET_SD_DATA, sizeof(pkt));
+	memcpy(pkt+PKT_HEADER_SIZE, block, 512);
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -131,16 +142,16 @@ int pkt_send_sd_data(struct sd *sd, uint8_t *block) {
  * PACKET_SD_CMD_ARG format (FPGA):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  1   | Register number (1, 2, 3, or 4), or 0 for the CMD byte
- *    10   |  1   | Value of the register or CMD number
+ *     0   |  11  | Header
+ *    11   |   1  | Register number (1, 2, 3, or 4), or 0 for the CMD byte
+ *    12   |   1  | Value of the register or CMD number
  */
 
 int pkt_send_sd_cmd_arg(struct sd *sd, uint32_t fpga_counter, uint8_t regnum, uint8_t val) {
-	char pkt[9+1+1];
-	pkt_set_header_fpga(sd, pkt, fpga_counter, PACKET_SD_CMD_ARG);
-	pkt[9] = regnum;
-	pkt[10] = val;
+	char pkt[PKT_HEADER_SIZE+1+1];
+	pkt_set_header_fpga(sd, pkt, fpga_counter, PACKET_SD_CMD_ARG, sizeof(pkt));
+	pkt[PKT_HEADER_SIZE+0] = regnum;
+	pkt[PKT_HEADER_SIZE+1] = val;
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -149,13 +160,13 @@ int pkt_send_sd_cmd_arg(struct sd *sd, uint32_t fpga_counter, uint8_t regnum, ui
  * PACKET_SD_RESPONSE format (FPGA):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  1   | The contents of the first byte that the card answered with
+ *     0   |  11  | Header
+ *    11   |   1  | The contents of the first byte that the card answered with
  */
 int pkt_send_sd_response(struct sd *sd, uint32_t fpga_counter, uint8_t byte) {
-	char pkt[9+1];
-	pkt_set_header_fpga(sd, pkt, fpga_counter, PACKET_SD_RESPONSE);
-	pkt[9] = byte;
+	char pkt[PKT_HEADER_SIZE+1];
+	pkt_set_header_fpga(sd, pkt, fpga_counter, PACKET_SD_RESPONSE, sizeof(pkt));
+	pkt[PKT_HEADER_SIZE+0] = byte;
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -164,13 +175,13 @@ int pkt_send_sd_response(struct sd *sd, uint32_t fpga_counter, uint8_t byte) {
  * PACKET_SD_CID format (CPU):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  16  | Contents of the card's CID
+ *     0   |  11  | Header
+ *    11   |  16  | Contents of the card's CID
  */
 int pkt_send_sd_cid(struct sd *sd, uint8_t cid[16]) {
-	char pkt[9+16];
-	pkt_set_header(sd, pkt, PACKET_SD_CID);
-	memcpy(pkt+9, cid, 16);
+	char pkt[PKT_HEADER_SIZE+16];
+	pkt_set_header(sd, pkt, PACKET_SD_CID, PKT_HEADER_SIZE);
+	memcpy(pkt+PKT_HEADER_SIZE, cid, 16);
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -179,13 +190,13 @@ int pkt_send_sd_cid(struct sd *sd, uint8_t cid[16]) {
  * PACKET_SD_CSD format (CPU):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  16  | Contents of the card's CSD
+ *     0   |  11  | Header
+ *    11   |  16  | Contents of the card's CSD
  */
 int pkt_send_sd_csd(struct sd *sd, uint8_t csd[16]) {
-	char pkt[9+16];
-	pkt_set_header(sd, pkt, PACKET_SD_CSD);
-	memcpy(pkt+9, csd, 16);
+	char pkt[PKT_HEADER_SIZE+16];
+	pkt_set_header(sd, pkt, PACKET_SD_CSD, sizeof(pkt));
+	memcpy(pkt+PKT_HEADER_SIZE, csd, 16);
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -194,17 +205,17 @@ int pkt_send_sd_csd(struct sd *sd, uint8_t csd[16]) {
  * PACKET_BUFFER_OFFSET format (CPU):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  1   | 1 if this is the read buffer, 2 if it's write
- *    10   |  4   | Offset of the current buffer pointer
+ *     0   |  11  | Header
+ *    11   |   1  | 1 if this is the read buffer, 2 if it's write
+ *    12   |   4  | Offset of the current buffer pointer
  */
 int pkt_send_buffer_offset(struct sd *sd, uint8_t buffertype, uint32_t offset) {
-	char pkt[9+1+4];
+	char pkt[PKT_HEADER_SIZE+1+4];
 	uint32_t real_offset;
-	pkt_set_header(sd, pkt, PACKET_BUFFER_OFFSET);
-	pkt[9] = buffertype;
+	pkt_set_header(sd, pkt, PACKET_BUFFER_OFFSET, sizeof(PKT_HEADER_SIZE));
 	real_offset = htonl(offset);
-	memcpy(pkt+10, &real_offset, sizeof(real_offset));
+	pkt[PKT_HEADER_SIZE+0] = buffertype;
+	memcpy(pkt+PKT_HEADER_SIZE+1, &real_offset, sizeof(real_offset));
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -213,15 +224,15 @@ int pkt_send_buffer_offset(struct sd *sd, uint8_t buffertype, uint32_t offset) {
  * PACKET_BUFFER_CONTENTS format (CPU):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  1   | 1 if this is the read buffer, 2 if it's write
- *    10   | 512  | Contents of the buffer
+ *     0   |  11  | Header
+ *    11   |  1   | 1 if this is the read buffer, 2 if it's write
+ *    12   | 512  | Contents of the buffer
  */
 int pkt_sent_buffer_contents(struct sd *sd, uint8_t buffertype, uint8_t *buffer) {
-	char pkt[9+1+512];
-	pkt_set_header(sd, pkt, PACKET_BUFFER_CONTENTS);
-	pkt[9] = buffertype;
-	memcpy(pkt+10, buffer, 512);
+	char pkt[PKT_HEADER_SIZE+1+512];
+	pkt_set_header(sd, pkt, PACKET_BUFFER_CONTENTS, sizeof(pkt));
+	pkt[PKT_HEADER_SIZE+0] = buffertype;
+	memcpy(pkt+PKT_HEADER_SIZE+1, buffer, 512);
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
 
@@ -230,17 +241,17 @@ int pkt_sent_buffer_contents(struct sd *sd, uint8_t buffertype, uint8_t *buffer)
  * PACKET_COMMAND format (CPU):
  *  Offset | Size | Description
  * --------+------+-------------
- *     0   |  9   | Header
- *     9   |  2   | Two-character command code
- *    11   |  4   | 32-bit command argument
+ *     0   |  11  | Header
+ *    11   |   2  | Two-character command code
+ *    13   |   4  | 32-bit command argument
  */
 int pkt_send_command(struct sd *sd, struct sd_cmd *cmd) {
-	char pkt[9+2+4];
+	char pkt[PKT_HEADER_SIZE+2+4];
 	uint32_t arg;
-	pkt_set_header(sd, pkt, PACKET_COMMAND);
-	pkt[9] = cmd->cmd[0];
-	pkt[10] = cmd->cmd[1];
 	arg = htonl(cmd->arg);
-	memcpy(pkt+9+2, &arg, sizeof(arg));
+	pkt_set_header(sd, pkt, PACKET_COMMAND, sizeof(pkt));
+	pkt[PKT_HEADER_SIZE+0] = cmd->cmd[0];
+	pkt[PKT_HEADER_SIZE+1] = cmd->cmd[1];
+	memcpy(pkt+PKT_HEADER_SIZE+2, &arg, sizeof(arg));
 	return net_write_data(sd, pkt, sizeof(pkt));
 }
