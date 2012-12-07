d97ab093fca06a8511c4b34123c151826588d0d9
Sean Cross <xobs@xoblo.gs>
2012-12-07 10:51:33 -0300

    sd: Fix marching zeroes/ones
    The 16- and 32-bit values didn't actually work.

--- sd.c
+++ sd.c
@@ -524,42 +524,56 @@ static int sd_net_copy_read_to_write_buffer(struct sd *state, int arg) {
 }
 
 static int sd_net_pattern_select(struct sd *state, int arg) {
-	int i;
+	int i, max;
+	int sz = sizeof(state->sd_write_bfr);
+	uint8_t *sd_write_bfr_8 = (uint8_t *)state->sd_write_bfr;
+	uint16_t *sd_write_bfr_16 = (uint16_t *)state->sd_write_bfr;
+	uint32_t *sd_write_bfr_32 = (uint32_t *)state->sd_write_bfr;
 
 	/* All zeroes */
 	if (arg == 0) {
-		memset(state->sd_write_bfr, 0, sizeof(state->sd_write_bfr));
+		memset(sd_write_bfr_8, 0, sz);
 	}
 
 	/* All ones */
 	else if (arg == 1) {
-		memset(state->sd_write_bfr, 0xff, sizeof(state->sd_write_bfr));
+		memset(sd_write_bfr_8, 0xff, sz);
 	}
+
 
 	/* Walking zeroes */
-	else if (arg == 2 || arg == 3 || arg == 4) {
-		int max;
-		if (arg == 2)
-			max = 8;
-		else if (arg == 3)
-			max = 16;
-		else
-			max = 32;
-		for (i=0; i<sizeof(state->sd_write_bfr); i++)
-			state->sd_write_bfr[i] = ~(1<<(i&(max-1)));
+	else if (arg == 2) {
+		max = 8;
+		for (i=0; i<sz/1; i++)
+			sd_write_bfr_8[i] = ~(1<<(i&(max-1)));
+	}
+	else if (arg == 3) {
+		max = 16;
+		for (i=0; i<sz/2; i++)
+			sd_write_bfr_16[i] = ~(1<<(i&(max-1)));
+	}
+	else if (arg == 4) {
+		max = 32;
+		for (i=0; i<sz/4; i++)
+			sd_write_bfr_32[i] = ~(1<<(i&(max-1)));
 	}
 
+
 	/* Walking ones */
-	else if (arg == 5 || arg == 6 || arg == 7) {
-		int max;
-		if (arg == 5)
-			max = 8;
-		else if (arg == 6)
-			max = 16;
-		else
-			max = 32;
-		for (i=0; i<sizeof(state->sd_write_bfr); i++)
-			state->sd_write_bfr[i] = 1<<(i&(max-1));
+	else if (arg == 5) {
+		max = 8;
+		for (i=0; i<sz/1; i++)
+			sd_write_bfr_8[i] = (1<<(i&(max-1)));
+	}
+	else if (arg == 6) {
+		max = 16;
+		for (i=0; i<sz/2; i++)
+			sd_write_bfr_16[i] = (1<<(i&(max-1)));
+	}
+	else if (arg == 7) {
+		max = 32;
+		for (i=0; i<sz/4; i++)
+			sd_write_bfr_32[i] = (1<<(i&(max-1)));
 	}
 
 	return 0;
